cd72f114953f64735214718385d510275332fce0
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-15 00:07:02 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_2.7/HomeTask_2.7.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int a, b;
+    cout << "Введите число: \n";
+    cin >> a;
+    for (int i = a - 1; i > 0; i--) {
+        b = i;
+        if (a % (b * b) == 0 && a % (b * b * b) != 0) {
+            cout << b << endl;
+        }
+    }
+}
+
